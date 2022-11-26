0807a4093ee439c46e1219307363e04eabcabe99
infi-null-0-local <119103652+Infi-null-0@users.noreply.github.com>
2022-11-26 16:37:27 +0530

    added a calc program

---
+++ calculator.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float num1,num2,choice;
+    char case_var;
+    printf("\nSIMPLE CALCULATOR\n");
+    while (choice)
+    {
+        printf("\nEnter number 1: ");
+        scanf("%f",&num1);
+        printf("Enter number 2: ");
+        scanf("%f",&num2);
+        printf("Select Operation: ( + , - , * , / ) : ");
+        scanf(" %c",&case_var);
+        switch(case_var)
+        {
+            case '+':
+                printf("%.2f + %.2f = %.2f",num1,num2,num1+num2);
+                break;
+            case '-':
+                printf("%.2f - %.2f = %.2f",num1,num2,num1-num2);
+                break;
+            case '*':
+                printf("%.2f * %.2f = %.2f",num1,num2,num1*num2);
+                break;
+            case '/':
+                printf("%.2f / %.2f = %.2f",num1,num2,num1/num2);
+                break;
+            default:
+                printf("Invalid");   
+        }
+        printf("\n\nDo you want to continue(0-no): ");
+        scanf("%f",&choice);
+    } 
+    printf("\nEND\n");
+    return 0;
+}
